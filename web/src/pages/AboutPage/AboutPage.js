069a4f63a4543c3c85765955da554cb31022ffdc
Frank Congson <fcongson@gmail.com>
2021-07-26 16:11:16 +1200

    Learn redwoodjs with jason

---
+++ web/src/pages/AboutPage/AboutPage.js
@@ -0,0 +1,12 @@
+import BlogLayout from 'src/layouts/BlogLayout'
+
+const AboutPage = () => {
+  return (
+    <BlogLayout>
+      <h1>About</h1>
+      <p>🎉</p>
+    </BlogLayout>
+  )
+}
+
+export default AboutPage
